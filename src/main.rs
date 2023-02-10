23bc3b61e3f1af20866d60165c1fc204671e6195
Marcus Rådell <marcus@radell.net>
2023-02-10 21:34:39 +0100

    create prisma migration and testing code for prisma client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+mod prisma;
+
+use std::error::Error;
+
+use prisma::PrismaClient;
+use uuid::Uuid;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let client = PrismaClient::_builder().build().await?;
+
+    client
+        .user()
+        .create(
+            Uuid::new_v4().to_string(),
+            "marcus@radell.net".to_string(),
+            vec![],
+        )
+        .exec()
+        .await?;
+
+    let rows = client.user().find_many(vec![]).exec().await?;
+
+    println!("{rows:?}");
+
+    Ok(())
 }
